c83cbb477022e778fe5527ed2f4b3fc17c78f844
Daniel.Teran <Daniel.Teran@cognizant.com>
2022-09-26 15:19:08 -0500

    REST API and filtering

---
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,58 @@
+package com.rest.webservices.restfulwebservices.user;
+
+import jakarta.servlet.Servlet;
+import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class UserResource {
+    private UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService){
+        this.userDaoService = userDaoService;
+    }
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+
+    }
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        User user = userDaoService.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException("User Does not Exist!");
+        }
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
+
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        userDaoService.deleteById(id);
+
+    }
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+         return ResponseEntity.created(location).build();
+
+    }
+}
